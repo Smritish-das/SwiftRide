96eafb1af84de83c06ea5508118c8b37376ffbb5
Xerneas <www.smrihs.1@gmail.com>
2025-05-27 11:26:11 +0530

    final Commit 3

--- Backend/db/db.js
+++ Backend/db/db.js
@@ -1,14 +1,5 @@
 const mongoose = require('mongoose');
 
-//--------------Older version of connectToDb function----------------- 
-
-// function connectToDb(){
-//     mongoose.connect(process.env.DB_CONNECT, {  useNewUrlParser: true, useUnifiedTopology:true}
-//             ,() =>{
-//                 console.log('Connected to DB');
-//             }).catch(err => console.log(err));
-// }
-
 function connectToDb() {
     mongoose.connect(process.env.DB_CONNECT)
         .then(() => {
